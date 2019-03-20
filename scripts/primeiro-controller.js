6b910915d15252a239cce564915dd6cb71a334b7
ElianMelo <elianmelomorais@gmail.com>
2019-03-20 14:55:50 -0300

    Troca os arquivos para os utilizados pelo professor

--- scripts/primeiro-controller.js
+++ scripts/primeiro-controller.js
@@ -1,27 +1,16 @@
-angular.module("aplicacao").controller("PrimeiroController", function($scope){
-    $scope.nome = "Elian Melo";
-    var nome = "Alguma coisa";
-
-    $scope.iniciado = true;
-
-    $scope.alunos = ["Aluno 1", "Aluno 2", "Aluno 3", "Aluno 4"];
-
-    $scope.hoje = new Date();
-
-    $scope.finalizar = function(){
-        $scope.iniciado = false;
-    };
-
-    $scope.iniciar = function(){
-        $scope.iniciado = true;
-    };
-
-    $scope.submeter = function() {
-        if ($scope.form_1.$valid) {
-            $scope.alunos.push($scope.nome_aluno);
-        } else {
-            alert('Preencha o formulário corretamente!');
-        }
-    }
-
+angular.module('aplicacao').controller('PrimeiroController', function($scope){
+	$scope.nome = 'Gabriel Pinheiro';
+	var nome = "Lá Lá Lá";
+	
+	$scope.iniciado = true;
+	
+	$scope.alunos = ['Aluno 1', 'Aluno 2', 'Aluno 3', 'Aluno 4'];
+	
+	$scope.finalizar = function(){
+		$scope.iniciado = false;
+	};
+	
+	$scope.iniciar = function() {
+		$scope.iniciado = true;
+	};
 });
