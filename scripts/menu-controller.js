6f1e772d6524bd887b6b174aef3195efd63310c7
ElianMelo <elianmelomorais@gmail.com>
2019-03-21 17:03:30 -0300

    Adiciona comentario padrao

--- scripts/menu-controller.js
+++ scripts/menu-controller.js
@@ -1,3 +1,9 @@
+/*
+Nome do autor: Elian Melo Morais
+Data de criação do arquivo: 21/03/2019
+Objetivo sucinto do programa: Controller que controla o menu da aplicacao
+Referência ao enunciado/origem do exercício: https://cursos.timtec.com.br/course/desenvolvimento-web-com-angularjs/intro
+*/
 angular.module('aplicacao').controller('MenuController', function($scope, $state) {
 
     $scope.irInicio = function() {
